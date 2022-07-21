d06775e5d52d5e93cf97fe385e974e2fd2f0fb79
Gavin Lin <gavinjwl@amazon.com>
2022-07-21 14:23:16 +0800

    Redesign dashboard layout and time period

--- clickstream/stacks/dashboard_stack.py
+++ clickstream/stacks/dashboard_stack.py
@@ -1,5 +1,5 @@
 from aws_cdk import Duration, Stack
-from aws_cdk.aws_cloudwatch import Dashboard, GraphWidget, PeriodOverride, Metric, SingleValueWidget, MathExpression, YAxisProps
+from aws_cdk.aws_cloudwatch import Dashboard, GraphWidget, PeriodOverride, Metric, SingleValueWidget, MathExpression, YAxisProps, Unit
 from constructs import Construct
 from aws_cdk.aws_apigateway import LambdaRestApi
 from aws_cdk.aws_lambda import Function as LambdaFunction
@@ -20,7 +20,7 @@ class DashboardStack(Stack):
         dashboard = Dashboard(
             self, id='ClickstreamDashboard',
             dashboard_name='Clickstream',
-            period_override=PeriodOverride.AUTO,
+            period_override=PeriodOverride.INHERIT,
             start='-PT3H',
         )
 
@@ -32,18 +32,23 @@ class DashboardStack(Stack):
                 metrics=[
                     clickstream_backend_api.metric_latency(
                         label='Latency(p95)', statistic='p95',
+                        period=Duration.minutes(1)
                     ),
                     clickstream_backend_api.metric_integration_latency(
                         label='IntegrationLatency(p95)', statistic='p95',
+                        period=Duration.minutes(1)
                     ),
                     clickstream_backend_api.metric_count(
                         statistic='Sum',
+                        period=Duration.minutes(1)
                     ),
                     clickstream_backend_api.metric_client_error(
                         statistic='Sum',
+                        period=Duration.minutes(1)
                     ),
                     clickstream_backend_api.metric_server_error(
                         statistic='Sum',
+                        period=Duration.minutes(1)
                     ),
                 ],
             ),
@@ -53,15 +58,19 @@ class DashboardStack(Stack):
                 metrics=[
                     clickstream_backend_function.metric_invocations(
                         statistic='Sum',
+                        period=Duration.minutes(1)
                     ),
                     clickstream_backend_function.metric_duration(
                         label='Duration(p95)', statistic='p95',
+                        period=Duration.minutes(1)
                     ),
                     clickstream_backend_function.metric_throttles(
                         statistic='Sum',
+                        period=Duration.minutes(1)
                     ),
                     clickstream_backend_function.metric_errors(
                         statistic='Sum',
+                        period=Duration.minutes(1)
                     ),
                 ],
             ),
@@ -69,15 +78,21 @@ class DashboardStack(Stack):
                 title='KinesisStream',
                 width=8, height=6,
                 metrics=[
-                    kinesis_stream.metric_put_records_success(
-                        statistic='sum'),
-                    kinesis_stream.metric_get_records_success(
-                        statistic='sum'),
+                    kinesis_stream.metric_incoming_records(
+                        statistic='sum',
+                        period=Duration.minutes(1)
+                    ),
+                    kinesis_stream.metric_get_records(
+                        statistic='sum',
+                        period=Duration.minutes(1)
+                    ),
                     kinesis_stream.metric_put_records_latency(
                         label='PutRecords.Latency(p95)', statistic='p95',
+                        period=Duration.minutes(1)
                     ),
                     kinesis_stream.metric_get_records_latency(
-                        label='PutRecords.Latency(p95)', statistic='p95',
+                        label='GetRecords.Latency(p95)', statistic='p95',
+                        period=Duration.minutes(1)
                     )
                 ]
             )
@@ -90,6 +105,8 @@ class DashboardStack(Stack):
                 'Workgroup': 'clickstream-workgroup'
             },
             statistic='sum',
+            period=Duration.minutes(1),
+            unit=Unit.COUNT,
         )
         metric_compute_seconds = Metric(
             namespace='AWS/Redshift-Serverless',
@@ -98,14 +115,8 @@ class DashboardStack(Stack):
                 'Workgroup': 'clickstream-workgroup'
             },
             statistic='sum',
-        )
-        metric_data_storage = Metric(
-            namespace='AWS/Redshift-Serverless',
-            metric_name='DataStorage',
-            dimensions_map={
-                'Namespace': 'clickstream-namespace'
-            },
-            statistic='sum',
+            period=Duration.minutes(1),
+            unit=Unit.SECONDS,
         )
 
         compute_cost = MathExpression(
@@ -113,30 +124,26 @@ class DashboardStack(Stack):
             expression='sum_compute_seconds / 60 / 60 * 0.494',
             using_metrics={
                 'sum_compute_seconds': metric_compute_seconds
-            }
-        )
-        storage_cost = MathExpression(
-            label='StorageCost',
-            expression='sum_data_storage / 1024 * 0.0261',
-            using_metrics={
-                'sum_data_storage': metric_data_storage
-            }
+            },
+            period=Duration.minutes(1)
         )
 
         # row 2
         dashboard.add_widgets(
             GraphWidget(
-                title='RedshiftServerless - Compute',
+                title='RPU resource',
                 width=12, height=6,
-                left=[metric_compute_capacity, metric_compute_seconds],
-                right=[compute_cost],
-                right_y_axis=YAxisProps(label='PerComputeCost (USD)')
+                left=[metric_compute_capacity],
+                left_y_axis=YAxisProps(label='RPU', show_units=False),
+                right=[metric_compute_seconds],
+                right_y_axis=YAxisProps(label='Seconds', show_units=False)
             ),
             GraphWidget(
-                title='RedshiftServerless - Storage',
+                title='RPU cost',
                 width=12, height=6,
-                left=[metric_data_storage],
-                right=[storage_cost],
-                right_y_axis=YAxisProps(label='CurrentStorageCost (USD)')
+                left=[metric_compute_seconds],
+                left_y_axis=YAxisProps(label='Seconds', show_units=False),
+                right=[compute_cost],
+                right_y_axis=YAxisProps(label='USD', show_units=False)
             )
         )
