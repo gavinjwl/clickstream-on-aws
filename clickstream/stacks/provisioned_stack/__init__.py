5c3cd4c5de633186bcc3cc2d14494bca1cafa38d
Gavin Lin <gavin.jwlin@gmail.com>
2023-03-29 15:10:22 +0000

    initial provisioned stack

---
+++ clickstream/stacks/provisioned_stack/__init__.py
@@ -0,0 +1,7 @@
+from clickstream.stacks.provisioned_stack.api import ECSStack
+from clickstream.stacks.provisioned_stack.core import RedshiftStreamingIngestionStack
+
+__all__ = [
+    'RedshiftStreamingIngestionStack',
+    'ECSStack'
+]
